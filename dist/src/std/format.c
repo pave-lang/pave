62ae61ada26b8ccfa850e824313cf5efe1608c6e
logan-gyxos <logan@gyxos.com>
2026-05-24 11:01:36 +1200

    Improve format function

--- dist/src/std/format.c
+++ dist/src/std/format.c
@@ -20,90 +20,88 @@ void format(struct trait_Writer out, struct str fmt, struct slice_ref_trait_Form
         uintptr_t start = i;
 
         #line 12 "src/std/Format.pv"
-        while (i < fmt.length && fmt.ptr[i] != '{' && fmt.ptr[i] != '}') {
+        while (i < fmt.length) {
             #line 13 "src/std/Format.pv"
-            i += 1;
+            if (fmt.ptr[i] == '{') {
+                #line 14 "src/std/Format.pv"
+                if (i + 1 < fmt.length && fmt.ptr[i + 1] == '{') {
+                    #line 15 "src/std/Format.pv"
+                    out.vtable->fn_write(out.instance, str__slice(fmt, start, i + 1));
+                    #line 16 "src/std/Format.pv"
+                    i += 2;
+                    #line 17 "src/std/Format.pv"
+                    start = i;
+                } else {
+                    #line 19 "src/std/Format.pv"
+                    break;
+                }
+            } else if (fmt.ptr[i] == '}' && i + 1 < fmt.length && fmt.ptr[i + 1] == '}') {
+                #line 22 "src/std/Format.pv"
+                out.vtable->fn_write(out.instance, str__slice(fmt, start, i + 1));
+                #line 23 "src/std/Format.pv"
+                i += 2;
+                #line 24 "src/std/Format.pv"
+                start = i;
+            } else {
+                #line 26 "src/std/Format.pv"
+                i += 1;
+            }
         }
 
-        #line 16 "src/std/Format.pv"
+        #line 30 "src/std/Format.pv"
         if (start < i) {
-            #line 17 "src/std/Format.pv"
+            #line 31 "src/std/Format.pv"
             out.vtable->fn_write(out.instance, str__slice(fmt, start, i));
         }
 
-        #line 20 "src/std/Format.pv"
+        #line 34 "src/std/Format.pv"
         if (i >= fmt.length) {
-            #line 21 "src/std/Format.pv"
+            #line 35 "src/std/Format.pv"
             return;
         }
 
-        #line 24 "src/std/Format.pv"
-        if (fmt.ptr[i] == '}') {
-            #line 25 "src/std/Format.pv"
-            if (i + 1 < fmt.length && fmt.ptr[i + 1] == '}') {
-                #line 26 "src/std/Format.pv"
-                out.vtable->fn_write_char(out.instance, '}');
-                #line 27 "src/std/Format.pv"
-                i += 2;
-            } else {
-                #line 29 "src/std/Format.pv"
-                out.vtable->fn_write_char(out.instance, fmt.ptr[i]);
-                #line 30 "src/std/Format.pv"
-                i += 1;
-            }
-
-            #line 33 "src/std/Format.pv"
-            continue;
-        }
-
-        #line 36 "src/std/Format.pv"
-        if (i + 1 < fmt.length && fmt.ptr[i + 1] == '{') {
-            #line 37 "src/std/Format.pv"
-            out.vtable->fn_write_char(out.instance, '{');
-            #line 38 "src/std/Format.pv"
-            i += 2;
-            #line 39 "src/std/Format.pv"
-            continue;
-        }
-
-        #line 42 "src/std/Format.pv"
+        #line 38 "src/std/Format.pv"
         uintptr_t end = i + 1;
 
-        #line 44 "src/std/Format.pv"
+        #line 40 "src/std/Format.pv"
         while (end < fmt.length && fmt.ptr[end] != '}') {
-            #line 45 "src/std/Format.pv"
+            #line 41 "src/std/Format.pv"
             end += 1;
         }
 
-        #line 48 "src/std/Format.pv"
+        #line 44 "src/std/Format.pv"
         if (end >= fmt.length) {
-            #line 49 "src/std/Format.pv"
+            #line 45 "src/std/Format.pv"
             out.vtable->fn_write_char(out.instance, fmt.ptr[i]);
-            #line 50 "src/std/Format.pv"
+            #line 46 "src/std/Format.pv"
             i += 1;
-            #line 51 "src/std/Format.pv"
+            #line 47 "src/std/Format.pv"
             continue;
         }
 
-        #line 54 "src/std/Format.pv"
+        #line 50 "src/std/Format.pv"
         struct str info = str__slice(fmt, i + 1, end);
 
-        #line 56 "src/std/Format.pv"
-        if (part_i < parts.length) {
-            #line 57 "src/std/Format.pv"
-            parts.data[part_i].vtable->fn_format(parts.data[part_i].instance, out, info);
-            #line 58 "src/std/Format.pv"
-            part_i += 1;
-        } else {
-            #line 60 "src/std/Format.pv"
+        #line 52 "src/std/Format.pv"
+        if (part_i >= parts.length) {
+            #line 53 "src/std/Format.pv"
             out.vtable->fn_write_char(out.instance, '{');
-            #line 61 "src/std/Format.pv"
+            #line 54 "src/std/Format.pv"
             out.vtable->fn_write(out.instance, info);
-            #line 62 "src/std/Format.pv"
+            #line 55 "src/std/Format.pv"
             out.vtable->fn_write_char(out.instance, '}');
+            #line 56 "src/std/Format.pv"
+            i = end + 1;
+            #line 57 "src/std/Format.pv"
+            continue;
         }
 
-        #line 65 "src/std/Format.pv"
+        #line 60 "src/std/Format.pv"
+        parts.data[part_i].vtable->fn_format(parts.data[part_i].instance, out, info);
+        #line 61 "src/std/Format.pv"
+        part_i += 1;
+
+        #line 63 "src/std/Format.pv"
         i = end + 1;
     }
 }
